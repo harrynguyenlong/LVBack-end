96436300b8e497a9cd03dc1f0defa0b0da9365ce
Viet Tran <tranhuyviet@gmail.com>
2020-04-11 13:33:47 +0300

    implemented PostForm

--- client/src/components/Home/PostForm.js
+++ client/src/components/Home/PostForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
     Dialog,
     DialogTitle,
@@ -36,6 +36,23 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center',
         color: theme.palette.common.colorWhite,
     },
+    contentText: {
+        width: '100%',
+        maxWidth: '100%',
+        minWidth: '100%',
+        height: '50px',
+        minHeight: '50px',
+        outline: 'none',
+        fontSize: '14px',
+        paddingLeft: '6px',
+        paddingTop: '4px',
+        borderRadius: '5px',
+        border: `1px solid ${theme.palette.common.colorGreyLight}`,
+        background: theme.palette.common.colorLightBg,
+        '&:focus': {
+            border: `1px solid ${theme.palette.common.colorGreen}`,
+        },
+    },
     dropzone: {
         margin: '20px 0',
     },
@@ -54,9 +71,33 @@ const useStyles = makeStyles((theme) => ({
 const PostForm = ({ isPostFormOpen, handlePostFormClose }) => {
     const classes = useStyles();
     const [imageUpload, setImageUpload] = useState([]);
+    const contentTextRef = useRef();
 
     const handeChangeImageUpload = (files) => {
         setImageUpload(files);
+        console.log(files);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submited...');
+        console.log('content text:', contentTextRef.current.value);
+        console.log('image upload', imageUpload);
+        const formData = new FormData();
+        formData.append('contentText', contentTextRef.current.value);
+        formData.append('postImageUrl', imageUpload[0]);
+
+        console.log('form data', formData);
+
+        // const requestBody = {
+        //     query: `
+        //         mutation {
+        //             createPost(
+        //                 contentText:
+        //             )
+        //         }
+        //     `
+        // }
     };
 
     return (
@@ -66,45 +107,54 @@ const PostForm = ({ isPostFormOpen, handlePostFormClose }) => {
                 onClose={handlePostFormClose}
                 aria-labelledby="customized-dialog-title"
             >
-                <DialogTitle
-                    id="customized-dialog-title"
-                    onClose={handlePostFormClose}
-                    className={classes.dialogTitleWrapper}
-                >
-                    <div className={classes.dialogTitle}>
-                        <Typography style={{ fontWeight: '500' }}>Create New Post</Typography>
+                <form onSubmit={handleSubmit}>
+                    <DialogTitle
+                        id="customized-dialog-title"
+                        onClose={handlePostFormClose}
+                        className={classes.dialogTitleWrapper}
+                    >
+                        <div className={classes.dialogTitle}>
+                            <Typography style={{ fontWeight: '500' }}>Create New Post</Typography>
 
-                        <IconButton onClick={handlePostFormClose}>
-                            <CloseIcon style={{ color: 'white' }} />
-                        </IconButton>
-                    </div>
-                </DialogTitle>
-                <DialogContent dividers style={{ minWidth: '600px' }}>
-                    {/* <DialogContentText>Please fill out the form below</DialogContentText> */}
-                    <TextField
-                        autoFocus
-                        multiline
-                        rows="4"
-                        name="content"
-                        label="Content"
-                        variant="outlined"
-                        style={{ width: '100%' }}
-                    />
-                    <div className={classes.dropzone}>
-                        <DropzoneArea
-                            onChange={handeChangeImageUpload}
-                            acceptedFiles={['image/*']}
+                            <IconButton onClick={handlePostFormClose}>
+                                <CloseIcon style={{ color: 'white' }} />
+                            </IconButton>
+                        </div>
+                    </DialogTitle>
+                    <DialogContent dividers style={{ minWidth: '600px' }}>
+                        {/* <TextField
+                            autoFocus
+                            multiline
+                            rows="4"
+                            name="content"
+                            label="Content"
+                            variant="outlined"
+                            style={{ width: '100%' }}
+                            ref={contentTextRef}
+                        /> */}
+                        <textarea
+                            ref={contentTextRef}
+                            row={4}
+                            placeholder="The text content"
+                            className={classes.contentText}
                         />
-                    </div>
-                </DialogContent>
-                <DialogActions style={{ padding: '15px 0' }}>
-                    <button className={classes.button} onClick={handlePostFormClose}>
-                        Cancel
-                    </button>
-                    <button className={classes.button} onClick={handlePostFormClose}>
-                        Post
-                    </button>
-                </DialogActions>
+                        <div className={classes.dropzone}>
+                            <DropzoneArea
+                                onChange={handeChangeImageUpload}
+                                acceptedFiles={['image/*']}
+                                fileLimit={1}
+                            />
+                        </div>
+                    </DialogContent>
+                    <DialogActions style={{ padding: '15px 0' }}>
+                        <button className={classes.button} onClick={handlePostFormClose}>
+                            Cancel
+                        </button>
+                        <button type="submit" className={classes.button}>
+                            Post
+                        </button>
+                    </DialogActions>
+                </form>
             </Dialog>
         </div>
     );
